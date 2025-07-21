ff3c930a132085acd8d2246f32afc32eb1a6a74d
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2025-07-21 14:10:51 +0700

    fix telegram

--- src/main/java/com/tanghai/expense_tracker/service/TelegramService.java
+++ src/main/java/com/tanghai/expense_tracker/service/TelegramService.java
@@ -2,15 +2,12 @@ package com.tanghai.expense_tracker.service;
 
 import com.tanghai.expense_tracker.config.TelegramProperties;
 import com.tanghai.expense_tracker.constant.Static;
-import com.tanghai.expense_tracker.util.MessageFormatter;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,11 +22,6 @@ public class TelegramService {
     }
 
     public void sendMessage(String message) {
-        String telegramBotUrl = UriComponentsBuilder
-                .fromHttpUrl(Static.TELEGRAM_BOT_URL)
-                .path(telegramProperties.getBotToken())
-                .path(Static.TELEGRAM_BOT_URL)
-                .toUriString();
 
         Map<String, String> request = new HashMap<>();
         request.put(Static.CHAT_ID, telegramProperties.getChatId());
@@ -41,7 +33,10 @@ public class TelegramService {
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(request, headers);
 
         if(telegramProperties.isEnable()) {
-            restTemplate.postForEntity(telegramBotUrl, entity, String.class);
+            restTemplate.postForEntity(
+                    Static.TELEGRAM_BOT_URL + telegramProperties.getBotToken() + Static.SEND_MESSAGE,
+                    entity,
+                    String.class);
         }
     }
 
